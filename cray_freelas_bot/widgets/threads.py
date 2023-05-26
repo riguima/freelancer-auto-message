64edaf153b362066ab4244c0d70885a05e3cd519
riguima <richard.alexsander.guima@gmail.com>
2023-05-26 11:14:33 -0300

    fix: add text message to send_message

--- cray_freelas_bot/widgets/threads.py
+++ cray_freelas_bot/widgets/threads.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from pathlib import Path
 from time import sleep
 
@@ -7,6 +8,7 @@ from PySide6 import QtCore
 
 from cray_freelas_bot.common.project import (
     create_browser_from_module,
+    get_greeting_according_time,
     to_excel,
 )
 from cray_freelas_bot.domain.browser import IBrowser
@@ -23,7 +25,22 @@ class BrowserThread(QtCore.QThread):
                 projects_urls = browser.get_projects_urls(bot['category'])
                 for project_url in projects_urls:
                     if project_url not in urls:
-                        message = browser.send_message(project_url)
+                        project = browser.get_project(project_url)
+                        greeting = get_greeting_according_time(
+                            datetime.now().time()
+                        )
+                        text = (
+                            f'{greeting} {{b}}{project.client_name}{{/b}}, '
+                            f'tudo bem?\n\n Ao ler sobre o seu projeto '
+                            f'{{b}}"{project.name}"{{/b}}, percebi que ele '
+                            'está alinhado com a minha expertise em '
+                            f'{project.category}, gostaria de saber qual o '
+                            'seu prazo ideal para a conclusão do projeto e se '
+                            'você possui algum detalhe em específico que '
+                            'considera fundamental?\n\n'
+                            f'ass: {{b}}{browser.get_account_name()}{{/b}}'
+                        )
+                        message = browser.send_message(text, project_url)
                         to_excel([message], report_path)
             sleep(60)
 
