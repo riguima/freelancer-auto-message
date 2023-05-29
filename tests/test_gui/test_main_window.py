0c794bc289c746be84d5950cc89d7aeff9d5b6f6
riguima <richard.alexsander.guima@gmail.com>
2023-05-28 21:39:53 -0300

    add more tests for gui

--- tests/test_gui/test_main_window.py
+++ tests/test_gui/test_main_window.py
@@ -1,10 +1,31 @@
+import os
+
+import pytest
 from PySide6 import QtCore
 
 from cray_freelas_bot.widgets.main_window import MainWindow
 
 
-def test_show_bots_window(qtbot) -> None:
-    widget = MainWindow()
-    qtbot.addWidget(widget)
-    qtbot.mouseClick(widget.bots_button, QtCore.Qt.LeftButton)
-    assert widget.bots_window.isVisible()
+@pytest.fixture(scope='module')
+def main_window() -> MainWindow:
+    return MainWindow()
+
+
+def test_show_bots_window(qtbot, main_window: MainWindow) -> None:
+    qtbot.addWidget(main_window)
+    qtbot.mouseClick(main_window.bots_button, QtCore.Qt.LeftButton)
+    assert main_window.bots_window.isVisible()
+
+
+def test_run_without_bots(qtbot, main_window: MainWindow) -> None:
+    qtbot.addWidget(main_window)
+    os.remove('.bots.json')
+    qtbot.mouseClick(main_window.run_button, QtCore.Qt.LeftButton)
+    assert main_window.message_box.text() == 'Crie primeiro os bots'
+
+
+def test_run_without_bots(qtbot, main_window: MainWindow) -> None:
+    qtbot.addWidget(main_window)
+    os.remove('.bots.json')
+    qtbot.mouseClick(main_window.run_button, QtCore.Qt.LeftButton)
+    assert main_window.message_box.text() == 'Crie primeiro os bots'
